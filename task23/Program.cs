049950df22f35e204a5cc45c3efb0c1e4d087cca
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-06 19:57:17 +0300

    Add task23

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Program takes the positive integer number N as input and gives a table of cubes of numbers in range [1,N]
+
+
+int num = 0, cube = 0;
+int count = 1;
+
+while (num < 1)
+{
+    Console.Write("Enter the positive integer number: ");
+    num = int.Parse(Console.ReadLine());
+}
+
+while (count <= num)
+{
+    cube = count * count * count;
+    Console.Write($"{cube}, ");
+    count++;
+}
